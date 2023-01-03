7b9caa79cb8038b437523d8414a67898de45b39a
VitorLucas44 <vitoriosolucas@hotmail.com>
2023-01-03 09:54:35 +0100

    exo5

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,24 @@ function additionner(x, y) {
 
 
 // exo2
+function soustraction(x, y) {
+    console.log(x - y);
+}
+soustraction(5, 10);
+
+
+// exo3
+function multiplication(x, y) {
+    console.log(x * y);
+}
+multiplication(5, 10);
+
+
+// exo4
+function division(x, y) {
+    console.log(x / y);
+}
+division(5, 10);
+
+
+// exo5
